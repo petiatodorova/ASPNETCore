0b25fd841722cb70f1bca2a632d70ec04ad5c6f5
petiatodorova <invisioncl@gmail.com>
2019-01-19 22:01:21 +0200

    RazorPages Learning

---
+++ RazorPages/RazorPages/Data/RazorPagesContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesMovie.Models;
+
+namespace RazorPages.Models
+{
+    public class RazorPagesContext : DbContext
+    {
+        public RazorPagesContext (DbContextOptions<RazorPagesContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RazorPagesMovie.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ RazorPages/RazorPages/Pages/Movies/Create.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using RazorPages.Models;
+using RazorPagesMovie.Models;
+
+namespace RazorPages.Pages.Movies
+{
+    public class CreateModel : PageModel
+    {
+        private readonly RazorPages.Models.RazorPagesContext _context;
+
+        public CreateModel(RazorPages.Models.RazorPagesContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Movie Movie { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Movie.Add(Movie);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
